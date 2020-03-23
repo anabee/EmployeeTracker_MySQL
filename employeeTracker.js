a275143206ca14b6e239e85ce28ffad0d2fb3d00
Ana Valdivia <anav1317@yahoo.com>
2020-03-23 16:37:36 -0700

    Added a new starting menu

--- employeeTracker.js
+++ employeeTracker.js
@@ -26,16 +26,69 @@ connection.connect(function(err) {
 function start(){
     inquirer
         .prompt({
-            name: "action_start",
-            type: "list",
-            message: "What action would you like to take?",
-            choices: ["View All Employees",
-        "View All Employees by Department",
-        "View All Employees by Manager BONUS",
-        "Add Employee",
-        "Remove Employee BONUS",
-        "Update Employee Role",
-        "Update Employee Manager BONUS"
+        name: "start",
+        type: "list",
+        message: "Welcome, please add DEPARTMENTS and ROLES to begin. Once you have added your desired departments and roles, continue to the Employee Tab.",
+        choices: ["Add Department", "Add Role","Employees Tab","EXIT",
         ]
         })
+        .then(function(answer){
+            if (answer.start === "Add Department"){
+                addDepartment();
+            } else if (answer.start === "Add Role"){
+                addRole();
+            } else if (answer.start === "Employees Tab"){
+                startpartTwo();
+            } else{
+                connection.end();
+            }
+        })
+}
+
+
+function startpartTwo(){
+    inquirer
+        .prompt({
+        name: "action_start",
+        type: "list",
+        message: "What action would you like to take?",
+        choices: ["Add Employee",
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager BONUS",
+        "Remove Employee BONUS",
+        "Update Employee Role",
+        "Update Employee Manager BONUS",
+        "EXIT",
+        ]
+        })
+        .then(function(answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.action_start === "View All Employees") {
+                // console.table - to look better for a list of ees 
+                viewAllEes();
+            }
+            else if(answer.action_start === "View All Employees by Department") {
+                viewEesByDept();
+            }
+            else if(answer.action_start === "View All Employees by Manager BONUS") {
+                viewEesbyManager();
+            }
+            else if(answer.action_start === "Add Employee") {
+                // make sure to inquire about role and dept too
+                addEes();
+            }
+            else if(answer.action_start === "Remove Employee BONUS") {
+                removeEes();
+            }
+            else if(answer.action_start === "Update Employee Role") {
+                updateEeRole();
+            }
+            else if(answer.action_start === "Update Employee Manager BONUS") {
+                updateEeManager();
+            }
+             else{
+              connection.end();
+            }
+          });
 }
--- employeeTracker.sql
+++ employeeTracker.sql
@@ -15,7 +15,8 @@ CREATE TABLE role_table(
 	title VARCHAR(30), 
     slary DECIMAL(10,2), 
 	department_id INTEGER(10), 
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+    CONSTRAINT fk_department_table FOREIGN KEY (department_id) REFERENCES department_table(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee_table(
@@ -23,6 +24,8 @@ CREATE TABLE employee_table(
 	first_name VARCHAR(30), 
     last_name VARCHAR(30), 
 	role_id INTEGER(10), 
+    CONSTRAINT fk_role_table FOREIGN KEY (role_id) REFERENCES role_table(id) ON DELETE CASCADE,
 	manager_id INTEGER(10),
+    CONSTRAINT fk_employee_table FOREIGN KEY (manager_id) REFERENCES employee_table(id) ON DELETE SET NULL,
 	PRIMARY KEY (id)
 );
